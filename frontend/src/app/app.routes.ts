126b68947f323c7bf886bdd32174736c9a9ca917
Ramiro Rosales <ramirosales52@gmail.com>
2024-06-27 16:38:27 -0300

    list products table

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './template/home/home.component';
+import { ProductsComponent } from './products/products.component'
 import { TemplateComponent } from './template/template.component';
 import { AuthGuardService } from './auth-guard.service';
 
@@ -13,5 +14,6 @@ export const routes: Routes = [
     canActivate: [AuthGuardService],
   },
   { path: 'login', component: LoginComponent },
+  { path: 'products', component:  ProductsComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
